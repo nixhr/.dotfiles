5e23ddd19cef4e222d3c47b63c265f10cab7f13d
Nikola Pavkovic <nikola@iprojekt.hr>
2015-05-12 20:25:48 +0200

    added checking if files exist

--- install.sh
+++ install.sh
@@ -7,9 +7,7 @@ FILES=".bashrc .vimrc .tmux.conf .git-prompt-colors.sh"
 mkdir -p $BACKUP_DIR
 for i in $FILES
 do
-  echo -n "Moving $HOME/$i -> $BACKUP_DIR/$i ..."
-  mv $HOME/$i $BACKUP_DIR
-  echo "done."
+  [ -e $HOME/$i ] && {  echo -n "Moving $HOME/$i -> $BACKUP_DIR/$i ..." ; mv $HOME/$i $BACKUP_DIR; echo "done". ; }
   echo -n "Linking $DOTFILES_DIR/$i -> $HOME/$i ..."
   ln -s $DOTFILES_DIR/$i $HOME/$i
   echo "done."
